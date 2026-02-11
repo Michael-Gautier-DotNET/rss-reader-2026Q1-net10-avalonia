45c2800a2558a4da66a915d5b2473f61074f316d
Michael Gautier <mklinfinity@proton.me>
2026-02-10 19:42:26 -0600

    Immutable records

--- gautier.rss.data/Feed.cs
+++ gautier.rss.data/Feed.cs
@@ -1,18 +1,6 @@
 using System.Diagnostics;
 
-namespace gautier.rss.data
-{
-    [DebuggerDisplay($"{{{nameof(GetDebuggerDisplay)}(),nq}}")]
-    public class Feed
-    {
-        public int DbId { get; set; } = -1;
-        public string FeedName { get; set; } = string.Empty;
-        public string FeedUrl { get; set; } = string.Empty;
-        public string LastRetrieved { get; set; } = string.Empty;
-        public string RetrieveLimitHrs { get; set; } = string.Empty;
-        public string RetentionDays { get; set; } = string.Empty;
-        public string RowInsertDateTime { get; set; } = string.Empty;
+namespace gautier.rss.data;
 
-        private string GetDebuggerDisplay() => $"{FeedName} {FeedUrl} {LastRetrieved} {RetrieveLimitHrs} {RetentionDays}";
-    }
-}
+public record Feed(int DbId = -1, string FeedName = "", string FeedUrl = "", string LastRetrieved = "", string RetrieveLimitHrs = "", string RetentionDays = "", string RowInsertDateTime = "");
+
