a57ddfc51242fbf50e8d929c3e9e572235ebcfcb
Michael Gautier <mklinfinity@proton.me>
2026-02-10 19:41:56 -0600

    Revised iteration passes to favor immutable records

--- gautier.rss.data/FeedDataExchange.cs
+++ gautier.rss.data/FeedDataExchange.cs
@@ -102,17 +102,21 @@ namespace gautier.rss.data
             List<FeedArticleUnion> Articles = new();
             DateTime ModificationDateTime = DateTime.Now;
             string ModificationDateTimeText = ModificationDateTime.ToString(_InvariantFormat.UniversalSortableDateTimePattern);
-            feed.LastRetrieved = ModificationDateTimeText;
-            feed.RetrieveLimitHrs = "1";
-            feed.RetentionDays = "45";
+
             string filePath = GetNormalizedFeedFilePath(feedSaveDirectoryPath, feed);
 
             if (File.Exists(filePath))
             {
+                string ArticleDate = string.Empty;
+                string ArticleSummary = string.Empty;
+                string ArticleText = string.Empty;
+                string ArticleUrl = string.Empty;
+                string HeadlineText = string.Empty;
+                string RowInsertDateTime = string.Empty;
+
                 using StreamReader LineReader = new(filePath);
                 string FileLine = string.Empty;
                 string PreviousURL = string.Empty;
-                FeedArticleUnion FeedArticlePair = new();
                 bool InText = false;
                 List<string> LineHeaders = new()
                 {
@@ -146,48 +150,60 @@ namespace gautier.rss.data
 
                     if (Col1 == "SUM")
                     {
-                        FeedArticlePair.ArticleDetail.ArticleSummary = Col2;
+                        ArticleSummary = Col2;
                     }
 
                     if (Col1 == "TEXT")
                     {
                         InText = true;
-                        FeedArticlePair.ArticleDetail.ArticleText = Col2;
+                        ArticleText = Col2;
                     }
 
                     if (LineHeaders.Contains(Col1) == false && InText)
                     {
-                        FeedArticlePair.ArticleDetail.ArticleText += FileLine;
+                        ArticleText += FileLine;
                     }
 
                     if (Col1 == "URL" && PreviousURL != Col2)
                     {
-                        FeedArticlePair = new()
-                        {
-                            FeedHeader = feed,
-                            ArticleDetail = new()
-                            {
-                                FeedName = feed.FeedName,
-                            },
-                        };
-                        FeedArticlePair.ArticleDetail.RowInsertDateTime = ModificationDateTimeText;
-                        Articles.Add(FeedArticlePair);
-                        PreviousURL = Col2;
-                    }
+                        FeedArticleUnion FeedArticlePair = new
+                                    (
+                                        FeedHeader: feed,
+                                        ArticleDetail: new()
+                                        {
+                                            FeedName = feed.FeedName,
+                                            HeadlineText = HeadlineText,
+                                            ArticleSummary = ArticleSummary,
+                                            ArticleText = ArticleText,
+                                            ArticleDate = ArticleDate,
+                                            ArticleUrl = ArticleUrl,
+                                            RowInsertDateTime = RowInsertDateTime
+                                        }
+                                    );
 
-                    if (Col1 == "URL")
-                    {
-                        FeedArticlePair.ArticleDetail.ArticleUrl = Col2;
+                        if (!string.IsNullOrWhiteSpace(HeadlineText) && (!string.IsNullOrWhiteSpace(ArticleText) || !string.IsNullOrWhiteSpace(ArticleSummary)))
+                        {
+                            Articles.Add(FeedArticlePair);
+                        }
+
+                        ArticleDate = string.Empty;
+                        ArticleSummary = string.Empty;
+                        ArticleText = string.Empty;
+                        ArticleUrl = Col2;
+                        HeadlineText = string.Empty;
+                        RowInsertDateTime = ModificationDateTimeText;
+
+                        PreviousURL = Col2;
                     }
 
                     if (Col1 == "DATE")
                     {
-                        FeedArticlePair.ArticleDetail.ArticleDate = Col2;
+                        ArticleDate = Col2;
                     }
 
                     if (Col1 == "HEAD")
                     {
-                        FeedArticlePair.ArticleDetail.HeadlineText = Col2;
+                        HeadlineText = Col2;
                     }
                 }
             }
@@ -325,76 +341,6 @@ namespace gautier.rss.data
                 else
                 {
                     FeedWriter.ModifyFeed(sqlConn, feedHeader);
-                }
-            }
-
-            return;
-        }
-
-        public static Feed[] MergeFeedEntries(string feedDbFilePath, Feed[] feedEntries)
-        {
-            string ConnectionString = SQLUtil.GetSQLiteConnectionString(feedDbFilePath, 3);
-            using SQLiteConnection SQLConn = SQLUtil.OpenSQLiteConnection(ConnectionString);
-            List<string> FeedUrls = new();
-            List<Feed> StaticFeedEntries = new(feedEntries);
-            List<Feed> FeedEntries = FeedReader.GetAllRows(SQLConn);
-
-            if (FeedEntries.Count > 0)
-            {
-                /*Feed entries from the database.*/
-                MergeValidateFeedEntries(FeedEntries, StaticFeedEntries, FeedUrls);
-                /*Feed entries from the file.*/
-                MergeValidateFeedEntries(StaticFeedEntries, FeedEntries, FeedUrls);
-            }
-
-            else
-            {
-                FeedEntries = StaticFeedEntries;
-            }
-
-            return FeedEntries.ToArray();
-        }
-
-        private static void MergeValidateFeedEntries(List<Feed> leftSideValues, List<Feed> rightSideValues, List<string> secondKeys)
-        {
-            foreach (Feed LeftEntry in leftSideValues)
-            {
-                string SecondKey = LeftEntry.FeedUrl.ToLower();
-
-                if (secondKeys.Contains(SecondKey))
-                {
-                    continue;
-                }
-
-                else
-                {
-                    secondKeys.Add(SecondKey);
-                }
-
-                /*Feed entries from the file.*/
-                foreach (Feed RightSideEntry in rightSideValues)
-                {
-                    if (LeftEntry.FeedName == RightSideEntry.FeedName)
-                    {
-                        string RightSideSecondKey = RightSideEntry.FeedUrl.ToLower();
-
-                        if (SecondKey != RightSideSecondKey && secondKeys.Contains(RightSideSecondKey) == false)
-                        {
-                            /*
-                             * Give another feed the opportunity to use the url under a different feed name.
-                             * I do not really support this functionality but I am annotating the concept regardless.
-                             */
-                            secondKeys.Remove(RightSideSecondKey);
-                            /*
-                             * For now, assume the url from the secondary source is more current.
-                             *      In the future,
-                             *          may want to compare the
-                             *              file's last modifed time to the data record's last retrieved time.
-                             */
-                            LeftEntry.FeedUrl = RightSideSecondKey;
-                            secondKeys.Add(RightSideSecondKey);
-                        }
-                    }
                 }
             }
 
